da0ff8aa9c58c1abd0d4d95608752c8c70395129
DELL <nickorovkinwork@gmail.com>
2023-01-06 18:55:38 +0200

    feat: Add Pydantic model

--- api.py
+++ api.py
@@ -1,7 +1,18 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from pydantic import BaseModel
 
 app = FastAPI()
 
+class User(BaseModel):
+    username: str
+    password: str
+
 @app.get("/")
 async def root() -> dict:
-    return {'message': 'Hello, World!'}
+    return {"message": "Hello, World!"}
+
+@app.post("/register")
+async def register(user_json: User) -> dict:
+    user = user_json.dict()
+    
+    return {"message": "Got data"}
