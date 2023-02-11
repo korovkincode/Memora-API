455be8f61122034668e54e345ac2a5d337cfe017
DELL <nickorovkinwork@gmail.com>
2023-02-11 20:16:52 +0200

    feat: Finish Post CRUD Tests

--- tests/test_api.py
+++ tests/test_api.py
@@ -29,8 +29,8 @@ def test_signup():
 def test_auth():
     data = {"username": "test1", "password": "1234"}
     response = client.post("/api/login/", json=data)
-    assert response.status_code == 200
-    assert response.json() == {"message": "No such user"}
+    assert response.status_code == 404
+    assert response.json() == {"detail": "No such user!"}
     data["username"] = "test"
     response = client.post("/api/login/", json=data)
     assert response.status_code == 200
@@ -40,9 +40,11 @@ def test_auth():
 
 def test_createPost():
     data = {"data": "Hello, World!"}
+    response = client.post("/api/post/create/")
+    assert response.status_code == 422
     response = client.post("/api/post/create/", headers={"token": TOKEN + ","}, json=data)
-    assert response.status_code == 200
-    assert response.json() == {"message": "No such user"}
+    assert response.status_code == 404
+    assert response.json() == {"detail": "No such user!"}
     response = client.post("/api/post/create/", headers={"token": TOKEN}, json=data)
     assert response.status_code == 200
     assert response.json()["message"] == "/api/post/1/read/"
@@ -55,3 +57,46 @@ def test_readPost():
     response = client.get("/api/post/3/read")
     assert response.status_code == 200
     assert response.json() == {"message": "No token!"}
+    response = client.get("/api/post/3/read", headers={"token": "1234"})
+    assert response.status_code == 404
+    assert response.json() == {"detail": "No such post!"}
+    response = client.get("/api/post/1/read", headers={"token": "1234"})
+    assert response.status_code == 200
+    assert response.json() == {"message": "Wrong token!"}
+    response = client.get("/api/post/1/read", headers={"token": TOKEN})
+    assert response.status_code == 200
+    assert response.json() == {"message": "Hello, World!"}
+
+def test_updatePost():
+    data = {"data": "Hello!"}
+    response = client.put("/api/post/3/update/")
+    assert response.status_code == 422
+    response = client.put("/api/post/3/update/", json=data)
+    assert response.status_code == 404
+    assert response.json() == {"detail": "No such post!"}
+    response = client.put("/api/post/1/update/", json=data)
+    assert response.status_code == 200
+    assert response.json() == {"message": "Wrong token!"}
+    response = client.put("/api/post/1/update", headers={"token": TOKEN}, json=data)
+    assert response.status_code == 200
+    assert response.json() == {"message": "/api/post/1/read/"}
+    response = client.get("/api/post/1/read/", headers={"token": TOKEN})
+    assert response.status_code == 200
+    assert response.json() == {"message": "Hello!"}
+
+def test_deletePost():
+    response = client.delete("/api/post/3/delete/")
+    assert response.status_code == 200
+    assert response.json() == {"message": "No token!"}
+    response = client.delete("/api/post/3/delete/", headers={"token": "1234"})
+    assert response.status_code == 404
+    assert response.json() == {"detail": "No such post!"}
+    response = client.delete("/api/post/1/delete/", headers={"token": "1234"})
+    assert response.status_code == 200
+    assert response.json() == {"message": "Wrong token!"}
+    response = client.delete("/api/post/1/delete/", headers={"token": TOKEN})
+    assert response.status_code == 200
+    assert response.json() == {"message": "Deleted post"}
+    response = client.get("/api/post/1/read/", headers={"token": TOKEN})
+    assert response.status_code == 404
+    assert response.json() == {"detail": "No such post!"}
