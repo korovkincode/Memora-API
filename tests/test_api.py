b0964d7084d3361a8e60099093d350822fe5afc8
DELL <nickorovkinwork@gmail.com>
2023-02-13 22:30:17 +0200

    fix: Fix endpoints and tests

--- tests/test_api.py
+++ tests/test_api.py
@@ -22,7 +22,7 @@ def test_signup():
     response = client.post("/api/signup/", json=data)
     assert response.status_code == 200
     assert response.json() == {"message": "Add new user"}
-    response = client.post("/api/signup", json=data)
+    response = client.post("/api/signup/", json=data)
     assert response.status_code == 200
     assert response.json() == {"message": "User with this username already exists"}
 
@@ -40,80 +40,80 @@ def test_auth():
 
 def test_createPost():
     data = {"data": "Hello, World!"}
-    response = client.post("/api/post/create/")
+    response = client.post("/api/post/")
     assert response.status_code == 422
-    response = client.post("/api/post/create/", headers={"token": TOKEN + ","}, json=data)
+    response = client.post("/api/post/", headers={"token": TOKEN + ","}, json=data)
     assert response.status_code == 404
     assert response.json() == {"detail": "No such user!"}
-    response = client.post("/api/post/create/", headers={"token": TOKEN}, json=data)
+    response = client.post("/api/post/", headers={"token": TOKEN}, json=data)
     assert response.status_code == 200
-    assert response.json()["message"] == "/api/post/1/read/"
-    response = client.post("/api/post/create/file/", headers={"token": TOKEN},
+    assert response.json()["message"] == "/api/post/1/"
+    response = client.post("/api/post/file/", headers={"token": TOKEN},
     files={"file": ("picture.jpg", open("picture.jpg", "rb"), "image/jpeg")})
     assert response.status_code == 200
-    assert response.json()["message"] == "/api/post/2/read/"
+    assert response.json()["message"] == "/api/post/2/"
 
 def test_readPost():
-    response = client.get("/api/post/3/read")
+    response = client.get("/api/post/3/")
     assert response.status_code == 200
     assert response.json() == {"message": "No token!"}
-    response = client.get("/api/post/3/read", headers={"token": "1234"})
+    response = client.get("/api/post/3/", headers={"token": "1234"})
     assert response.status_code == 404
     assert response.json() == {"detail": "No such post!"}
-    response = client.get("/api/post/1/read", headers={"token": "1234"})
+    response = client.get("/api/post/1/", headers={"token": "1234"})
     assert response.status_code == 200
     assert response.json() == {"message": "Wrong token!"}
-    response = client.get("/api/post/1/read", headers={"token": TOKEN})
+    response = client.get("/api/post/1/", headers={"token": TOKEN})
     assert response.status_code == 200
     assert response.json() == {"message": "Hello, World!"}
 
 def test_updatePost():
     data = {"data": "Hello!"}
-    response = client.put("/api/post/3/update/")
+    response = client.put("/api/post/3/")
     assert response.status_code == 422
-    response = client.put("/api/post/3/update/", json=data)
+    response = client.put("/api/post/3/", json=data)
     assert response.status_code == 404
     assert response.json() == {"detail": "No such post!"}
-    response = client.put("/api/post/1/update/", json=data)
+    response = client.put("/api/post/1/", json=data)
     assert response.status_code == 200
     assert response.json() == {"message": "Wrong token!"}
-    response = client.put("/api/post/1/update", headers={"token": TOKEN}, json=data)
+    response = client.put("/api/post/1/", headers={"token": TOKEN}, json=data)
     assert response.status_code == 200
-    assert response.json() == {"message": "/api/post/1/read/"}
-    response = client.get("/api/post/1/read/", headers={"token": TOKEN})
+    assert response.json() == {"message": "/api/post/1/"}
+    response = client.get("/api/post/1/", headers={"token": TOKEN})
     assert response.status_code == 200
     assert response.json() == {"message": "Hello!"}
 
 def test_deletePost():
-    response = client.delete("/api/post/3/delete/")
+    response = client.delete("/api/post/3/")
     assert response.status_code == 200
     assert response.json() == {"message": "No token!"}
-    response = client.delete("/api/post/3/delete/", headers={"token": "1234"})
+    response = client.delete("/api/post/3/", headers={"token": "1234"})
     assert response.status_code == 404
     assert response.json() == {"detail": "No such post!"}
-    response = client.delete("/api/post/1/delete/", headers={"token": "1234"})
+    response = client.delete("/api/post/1/", headers={"token": "1234"})
     assert response.status_code == 200
     assert response.json() == {"message": "Wrong token!"}
-    response = client.delete("/api/post/1/delete/", headers={"token": TOKEN})
+    response = client.delete("/api/post/1/", headers={"token": TOKEN})
     assert response.status_code == 200
     assert response.json() == {"message": "Deleted post"}
-    response = client.get("/api/post/1/read/", headers={"token": TOKEN})
+    response = client.get("/api/post/1/", headers={"token": TOKEN})
     assert response.status_code == 404
     assert response.json() == {"detail": "No such post!"}
 
 def test_postTags():
-    response = client.post("/api/post/1/tags/add/")
+    response = client.post("/api/post/1/tags/")
     assert response.status_code == 422
-    response = client.post("/api/post/1/tags/add/", json={"tags": ["Photo"]})
+    response = client.post("/api/post/1/tags/", json={"tags": ["Photo"]})
     assert response.status_code == 200
     assert response.json() == {"message": "No token!"}
-    response = client.post("/api/post/1/tags/add/", headers={"token": "1234"}, json={"tags": ["Photo"]})
+    response = client.post("/api/post/1/tags/", headers={"token": "1234"}, json={"tags": ["Photo"]})
     assert response.status_code == 404
     assert response.json() == {"detail": "Wrong token or post doesn't exist!"}
-    response = client.post("/api/post/2/tags/add/", headers={"token": TOKEN + "@"}, json={"tags": ["Photo"]})
+    response = client.post("/api/post/2/tags/", headers={"token": TOKEN + "@"}, json={"tags": ["Photo"]})
     assert response.status_code == 404
     assert response.json() == {"detail": "Wrong token or post doesn't exist!"}
-    response = client.post("/api/post/2/tags/add/", headers={"token": TOKEN}, json={"tags": ["Photo"]})
+    response = client.post("/api/post/2/tags/", headers={"token": TOKEN}, json={"tags": ["Photo"]})
     assert response.status_code == 200
     assert response.json() == {"message": "/api/post/2/tags/"}
     response = client.get("/api/post/1/tags/")
@@ -128,7 +128,7 @@ def test_postTags():
     response = client.get("/api/post/2/tags/", headers={"token": TOKEN})
     assert response.status_code == 200
     assert response.json() == {"tags": ["Photo"]}
-    response = client.post("/api/post/2/tags/add/", headers={"token": TOKEN}, json={"tags": ["Note"]})
+    response = client.post("/api/post/2/tags/", headers={"token": TOKEN}, json={"tags": ["Note"]})
     assert response.status_code == 200
     assert response.json() == {"message": "/api/post/2/tags/"}
     response = client.get("/api/post/2/tags/", headers={"token": TOKEN})
